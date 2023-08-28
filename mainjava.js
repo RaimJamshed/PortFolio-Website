d0bb7351389554094a95bb8dbfd09df7976eceb6
Raim <raimjamshed4@gmail.com>
2023-08-28 23:59:32 +0500

    First Commit

---
+++ mainjava.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('#text', {
+    strings: ["Full Stack Web developer", "Frontend Developer", "Python Developer", "ML Enthusiast"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
